d21f5d0c5d9bf0788b1a8214f0f8a5b949fddb88
Jefte <jefte.justino@gmail.com>
2019-10-20 19:14:02 -0300

    Feature de checkins de alunos adicionada

---
+++ src/app/controllers/CheckinController.js
@@ -0,0 +1,67 @@
+import { endOfDay, startOfDay, subDays } from 'date-fns';
+import { Op } from 'sequelize';
+import Student from '../models/Student';
+import Checkin from '../models/Checkin';
+
+class CheckinController {
+  async index(req, res) {
+    const student_id = req.params.id;
+    const { page = 1 } = req.query;
+
+    const student = await Student.findByPk(student_id);
+
+    if (!student) {
+      return res.status(401).json({ error: 'Student not found' });
+    }
+
+    const checkins = await Checkin.findAll({
+      where: { student_id },
+      offset: (page - 1) * 20,
+      limit: 20,
+      order: ['created_at'],
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['nome', 'email'],
+        },
+      ],
+    });
+
+    return res.json(checkins);
+  }
+
+  async store(req, res) {
+    const student_id = req.params.id;
+
+    const student = await Student.findByPk(student_id);
+
+    if (!student) {
+      return res.status(401).json({ error: 'Student not found' });
+    }
+
+    /**
+     * Checar quantidade de checkins
+     * nos ultimos 7 dias
+     */
+    const endDate = new Date();
+    const startDate = subDays(endDate, 7);
+    const checkins = await Checkin.findAndCountAll({
+      where: {
+        student_id,
+        created_at: {
+          [Op.between]: [startOfDay(startDate), endOfDay(endDate)],
+        },
+      },
+    });
+
+    if (checkins.count >= 5) {
+      return res.status(401).json({ error: 'Checkins limit reached' });
+    }
+
+    const checkin = await Checkin.create({ student_id });
+
+    return res.json(checkin);
+  }
+}
+export default new CheckinController();
--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,15 @@ import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
 import RegistrationController from './app/controllers/RegistrationController';
+import CheckinController from './app/controllers/CheckinController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/student', StudentController.store);
+routes.get('/students/:id/checkin', CheckinController.index);
+routes.post('/students/:id/checkin', CheckinController.store);
 
 routes.post('/session', SessionController.store);
 
